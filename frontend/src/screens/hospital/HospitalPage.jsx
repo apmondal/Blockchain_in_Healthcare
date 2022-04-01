b63a91e508a176fb0f5a1078c0c4702cd4dbcd0b
Shreejeeb <shreejeebkesh@gmail.com>
2022-04-01 12:33:54 +0530

    Hospital page

---
+++ frontend/src/screens/hospital/HospitalPage.jsx
@@ -0,0 +1,15 @@
+import { Button, Typography } from '@mui/material';
+import React from 'react';
+
+const HospitalPage = () => {
+  return (
+    <div>
+      <Button>Add Doctor</Button>
+      <Button>Add Patient</Button>
+      <Typography>Available Doctors</Typography>
+      doctors list
+    </div>
+  );
+};
+
+export default HospitalPage;
