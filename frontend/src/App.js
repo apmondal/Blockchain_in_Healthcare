d621b253796f05af5df9cb4f2c034110926f668e
Shreejeeb <shreejeebkesh@gmail.com>
2022-04-01 12:34:24 +0530

    add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,47 @@ import { useContext } from 'react';
 import './App.css';
 import HospitalRegistrationForm from './components/HospitalRegistration';
 import { HospitalContext } from './context/HospitalsContext';
-function App() {
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/system';
+import HospitalPage from './screens/hospital/HospitalPage';
+import HospitalLogIn from './components/HospitalLogIn';
+function RenderForm() {
   const { connectWallet, connectedAccount } = useContext(HospitalContext);
   return (
     <div>
       {connectedAccount === '' ? (
-        <Button onClick={connectWallet}>Connect</Button>
+        <Box
+          sx={{
+            width: '100vw',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Button
+            onClick={connectWallet}
+            variant="contained"
+            color="primary"
+            size="large"
+          >
+            Connect your metamask wallet
+          </Button>
+        </Box>
       ) : (
         <HospitalRegistrationForm />
       )}
     </div>
+  );
+}
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<RenderForm />}></Route>
+      <Route path="/login" element={<HospitalLogIn />}></Route>
+      <Route path="/register" element={<HospitalRegistrationForm />}></Route>
+      <Route path="/hospital" element={<HospitalPage />}></Route>
+    </Routes>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HospitalsProvider } from './context/HospitalsContext';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HospitalsProvider>
-      <App />
-    </HospitalsProvider>
+    <BrowserRouter>
+      <HospitalsProvider>
+        <App />
+      </HospitalsProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
