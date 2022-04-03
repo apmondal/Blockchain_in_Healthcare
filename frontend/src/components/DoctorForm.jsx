81d96117f21edee4e9de0919d4ed366ef2b5ac2d
Shreejeeb <shreejeebkesh@gmail.com>
2022-04-03 12:43:18 +0530

    add doctor svg

--- frontend/src/components/DoctorForm.jsx
+++ frontend/src/components/DoctorForm.jsx
@@ -11,6 +11,7 @@ import { useContext } from 'react';
 import { DoctorContext } from '../context/DoctorContext';
 import { useNavigate } from 'react-router-dom';
 import ArrowBackIosNewRoundedIcon from '@mui/icons-material/ArrowBackIosNewRounded';
+import { ReactComponent as DoctorSvg } from '../assets/doctor.svg';
 const DoctorForm = () => {
   const { formData, handleChange, addDoctor } = useContext(DoctorContext);
   const navigate = useNavigate();
@@ -24,95 +25,102 @@ const DoctorForm = () => {
   };
   return (
     <Box
-      sx={{
-        display: 'flex',
-        width: '100vw',
-        height: 'calc(100vh - 64px)',
-        flexDirection: 'column',
-        alignItems: 'center',
-        backgroundColor: 'aliceblue',
-      }}
+      sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
     >
-      <IconButton
-        onClick={goToDoctorsPage}
-        variant="text"
-        color="primary"
-        sx={{ position: 'absolute', top: 70, left: 15 }}
-        size="small"
+      <Box sx={{ width: '50%', marginLeft: 10 }}>
+        <DoctorSvg width="100%" height="100%" />
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          width: '100vw',
+          height: 'calc(100vh - 64px)',
+          flexDirection: 'column',
+          alignItems: 'center',
+          backgroundColor: 'aliceblue',
+        }}
       >
-        <ArrowBackIosNewRoundedIcon fontSize="16px" />
-        Back
-      </IconButton>
-      <Paper sx={{ marginTop: 6 }}>
-        <Box
-          component="form"
-          noValidate
-          autoComplete="off"
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            gap: 2,
-            padding: 2,
-            width: 500,
-            minHeight: 300,
-          }}
-          onSubmit={handleSubmit}
+        <IconButton
+          onClick={goToDoctorsPage}
+          variant="text"
+          color="primary"
+          sx={{ position: 'absolute', top: 70, left: 15 }}
+          size="small"
         >
-          <Typography variant="h5" fontWeight="bold" textAlign="center">
-            Enter details to add new Doctor
-          </Typography>
-          <TextField
-            type="text"
-            value={formData.name}
-            onChange={handleChange}
-            name="name"
-            label="Name"
-            variant="outlined"
-            fullWidth
-          />
-          <TextField
-            type="text"
-            value={formData.id}
-            onChange={handleChange}
-            name="id"
-            label="Doctor Id"
-            variant="outlined"
-            fullWidth
-          />
-          <TextField
-            type="text"
-            value={formData.specification}
-            onChange={handleChange}
-            name="specification"
-            label="Specification"
-            variant="outlined"
-            fullWidth
-          />
-          <TextField
-            type="email"
-            value={formData.email}
-            onChange={handleChange}
-            name="email"
-            label="Email"
-            variant="outlined"
-            fullWidth
-          />
-          <TextField
-            type="text"
-            value={formData.address}
-            onChange={handleChange}
-            name="address"
-            label="Address"
-            variant="outlined"
-            fullWidth
-          />
+          <ArrowBackIosNewRoundedIcon fontSize="16px" />
+          Back
+        </IconButton>
+        <Paper sx={{ marginTop: 6 }}>
+          <Box
+            component="form"
+            noValidate
+            autoComplete="off"
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              gap: 2,
+              padding: 2,
+              width: 500,
+              minHeight: 300,
+            }}
+            onSubmit={handleSubmit}
+          >
+            <Typography variant="h5" fontWeight="bold" textAlign="center">
+              Enter details to add new Doctor
+            </Typography>
+            <TextField
+              type="text"
+              value={formData.name}
+              onChange={handleChange}
+              name="name"
+              label="Name"
+              variant="outlined"
+              fullWidth
+            />
+            <TextField
+              type="text"
+              value={formData.id}
+              onChange={handleChange}
+              name="id"
+              label="Doctor Id"
+              variant="outlined"
+              fullWidth
+            />
+            <TextField
+              type="text"
+              value={formData.specification}
+              onChange={handleChange}
+              name="specification"
+              label="Specification"
+              variant="outlined"
+              fullWidth
+            />
+            <TextField
+              type="email"
+              value={formData.email}
+              onChange={handleChange}
+              name="email"
+              label="Email"
+              variant="outlined"
+              fullWidth
+            />
+            <TextField
+              type="text"
+              value={formData.address}
+              onChange={handleChange}
+              name="address"
+              label="Address"
+              variant="outlined"
+              fullWidth
+            />
 
-          <Button type="submit" variant="contained" color="primary" fullWidth>
-            Submit
-          </Button>
-        </Box>
-      </Paper>
+            <Button type="submit" variant="contained" color="primary" fullWidth>
+              Submit
+            </Button>
+          </Box>
+        </Paper>
+      </Box>
     </Box>
   );
 };
